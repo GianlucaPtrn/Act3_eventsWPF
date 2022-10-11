b068f1d6455893bc2856aef06f8ee76781d7ccb6
GianlucaPtrn <200188@site.asty-moulin.be>
2022-10-11 09:52:40 +0200

    Ajout Act3

---
+++ Act3_Events/Act3_Events/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Act3_Events
+{
+    public partial class MainWindow : Window{
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Btn_1.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+            Btn_Calc.MouseEnter += new MouseEventHandler(SurvolBtn);
+            Btn_Calc.MouseLeave += new MouseEventHandler(AfterSurvolBtn);
+
+        }
+
+        private bool EstEntier(string textEnter)
+        {
+            return int.TryParse(textEnter, out int number);
+        }
+
+        private void VerifTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text != "," && !EstEntier(e.Text) && e.Text != "-")
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                if (e.Text == "," || e.Text == "-")
+                {
+                    if (((TextBox)sender).Text.IndexOf(e.Text) > -1)
+                    {
+                        e.Handled = true;
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                    }
+                }
+            }
+        }
+
+        private void SurvolBtn(object sender, MouseEventArgs e)
+        {
+            Btn_v.Visibility = Visibility.Visible;
+            Btn_v.Background = Brushes.Blue;
+        }
+
+        private void AfterSurvolBtn(object sender, MouseEventArgs e)
+        {
+            Btn_v.Visibility = Visibility.Hidden;
+            Btn_v.Background = Brushes.Transparent;
+        }
+
+        private void ClickCalc(object sender, RoutedEventArgs e)
+        {
+            
+            string result;
+            int textbox1 = Convert.ToInt32(Btn_1.Text);
+            int textbox2 = Convert.ToInt32(Btn_2.Text);
+            int textbox3 = Convert.ToInt32(Btn_3.Text);
+            Projet mesOutils = new Projet();
+            mesOutils.ResoudreTrinome(textbox1, textbox2, textbox3, out result);
+
+            Affichage secondPage = new Affichage();
+            secondPage.TextResult.Text = secondPage.TextResult.Text + " " + result;
+            this.Visibility = Visibility.Hidden;
+            secondPage.Show();
+
+        }
+
+       
+    }
+}
---
+++ Act3_Events/Act3_Events/Projet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act3_Events
+{
+    class Projet
+    {
+        public void ResoudreTrinome(int a, int b, int c, out string result)
+        {
+            double Delta;
+            result = "";
+            Delta = (b*b) - 4*a*c;
+            double x1;
+            double x2;
+
+            if (Delta < 0)
+            {
+                result = "Il n'y a pas de racines";
+            }
+            if (Delta == 0)
+            {
+                x1 = -b / (2*a);
+                result = "x1 est égal à : " + x1;
+            }
+            if (Delta > 0)
+            {
+                x1 = (-b + Math.Sqrt(Delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(Delta)) / (2 * a);
+                result = "x1 est égal à : " + Math.Round(x1, 2) + " x2 est égal à : " + Math.Round(x2, 2);
+            }
+        }
+    }
+}
